456b1c7fc3b3d738d356b1a392ba28937f9342d1
yanjiaxuan <330544968@qq.com>
2021-07-04 16:55:42 +0800

    + initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const https = require('https')
+const url='https://www.kuaibozy.com/api.php/provide/vod/from/kbm3u8?ac=list&pg='
+let page = 1
+
+const finalData = {}
+
+function queryData(curPage) {
+    https.get(url + page, { timeout: 5000 }, res => {
+        let result = ''
+        res.on('data', data => {
+            result += data
+        })
+        res.on("end", () => {
+            let obj
+            try {
+                obj = JSON.parse(result)
+            } catch (e) {
+                console.log(`>>>>>>>>>>>>>>>>>>>> json解析失败, ${e.message}`)
+                queryData(curPage)
+                return
+            }
+            obj.list.forEach(item => {
+                finalData[item.vod_name] = item.vod_id
+            })
+            console.log(`第${curPage}页数据写入完成，当前最大页: ${page}`)
+            if (page < obj.pagecount + 10) {
+                page++
+                queryData(page)
+            } else if (page === obj.pagecount + 10) {
+                setTimeout(() => {
+                    fs.writeFileSync('./result.json', JSON.stringify(finalData))
+                    console.log('写入完成')
+                    process.exit(1)
+                }, 15000)
+            }
+        })
+    }).on("error", e => console.log(e.message))
+}
+
+queryData(page);
+
